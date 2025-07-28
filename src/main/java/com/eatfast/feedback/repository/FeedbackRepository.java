69d2640612d685b469cd223d16bf6caf6c4900e2
WEN <sera1106930@gmail.com>
2025-07-28 22:09:12 +0800

    Update FeedbackRepository.java

--- src/main/java/com/eatfast/feedback/repository/FeedbackRepository.java
+++ src/main/java/com/eatfast/feedback/repository/FeedbackRepository.java
@@ -9,10 +9,9 @@ import java.util.List;
 @Repository
 public interface FeedbackRepository extends JpaRepository<FeedbackEntity, Long> {
 
-	// Spring Data JPA 會根據方法名稱自動生成查詢
-	// 由於 FeedbackEntity 中的欄位是 "memberId"，所以方法名稱是 findByMemberId
+	
 	List<FeedbackEntity> findByMemberId(Long memberId);
 
-	// 同理，對應 "storeId" 欄位
+	// 對應 "storeId" 欄位
 	List<FeedbackEntity> findByStoreId(Long storeId);
 }
